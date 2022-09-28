f7b1b07b8e104d72bc3494985bbb19aaa26523dd
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-09-28 17:15:57 +0600

    Add(prismic): setup types & slices in prismic

---
+++ .slicemachine/assets/slices/Gallery/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Gallery';
+
+export default {
+  title: 'slices/Gallery'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"images":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1496181133206-80ce9b88a853"}}],"primary":{},"slice_type":"gallery","id":"_Default"}} />
+_Default.storyName = ''
---
+++ .slicemachine/assets/slices/RichContent/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/RichContent';
+
+export default {
+  title: 'slices/RichContent'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"content":[{"type":"paragraph","text":"Officia id magna magna fugiat aliquip labore nulla nostrud. Esse ullamco eu adipisicing esse voluptate eiusmod mollit dolore pariatur commodo.","spans":[]}]},"slice_type":"content","id":"_Default"}} />
+_Default.storyName = ''

2730d137a85d8f48d8f7dfef2d7307bacec46e3d
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-02 03:40:50 +0600

    Update(prismic-schema): add CodeBlock slice

---
+++ .slicemachine/assets/slices/CodeBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/CodeBlock';
+
+export default {
+  title: 'slices/CodeBlock'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"content":[{"type":"paragraph","text":"Est ullamco pariatur incididunt sit et duis fugiat velit aliqua anim. Minim duis excepteur non.","spans":[]}],"language":"graphql"},"slice_type":"code","id":"_Default"}} />
+_Default.storyName = ''

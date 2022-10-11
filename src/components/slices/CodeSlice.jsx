7231111dd15c3fb083fdb27629c863aba648f153
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-12 01:48:18 +0600

    Update(UI): add top margin & rounded corners

--- src/components/slices/CodeSlice.jsx
+++ src/components/slices/CodeSlice.jsx
@@ -4,6 +4,7 @@ import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 const CodeSlice = ({ slice }) => {
   return (
     <SyntaxHighlighter
+      className="mt-3 rounded-md p-3"
       language={slice.primary.language}
       children={slice.primary.content[0].text}
       style={nightOwl}

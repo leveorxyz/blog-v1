64191e37ac7b4f07a3dcd069e385a3bc964c9369
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-14 08:09:33 +0600

    Update(config): canonical url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  site: "https://www.blog.leveor.xyz",
 });

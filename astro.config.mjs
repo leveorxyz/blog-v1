6673d7a2e1396c88f3632c697778079e09b89ad6
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-14 08:06:32 +0600

    Remove(deps): react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from "@astrojs/react";
-
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()]
+  integrations: [tailwind(), svelte()],
 });

675d4f9a307f3fab12affe1c41ed176ec48c932f
Samnan Rahee <samnan.rahee.96@gmail.com>
2022-10-14 08:03:46 +0600

    Refactor: migrate CodeSlice to svelte

---
+++ src/components/slices/CodeSlice.svelte
@@ -0,0 +1,7 @@
+<script>
+  import {HighlightAuto} from "svelte-highlight";
+
+  export let slice;
+</script>
+
+<HighlightAuto class="mt-3 rounded-md overflow-x-scroll" code={slice.primary.content[0].text} />
